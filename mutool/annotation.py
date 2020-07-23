1a447f0623daefa7623238212536b955c26aef7c
YanGen <zorage@qq.com>
2020-07-23 15:39:19 +0800

    try

--- mutool/annotation.py
+++ mutool/annotation.py
@@ -1,4 +1,4 @@
-import time
+import time,sys
 # 几个方法增强
 # 打印日志
 def log(text="execute => "):
@@ -32,3 +32,19 @@ def retry(count=1):
             raise ex
         return wrapper
     return decorator
+
+
+def tryfun(printdebug=True):
+    def inner1(f):
+        def inner2(*args, **kwargs):
+            try:
+                res = f(*args, **kwargs)
+            except Exception as err:
+                if printdebug:
+                    info = sys.exc_info()[2].tb_frame.f_back
+                    temp = "filename:{}\nlines:{}\tfuncation:{}\terror:{}"
+                    print(temp.format(info.f_code.co_filename, info.f_lineno, f.__name__, repr(err)))
+                res = None
+            return res
+        return inner2
+    return inner1
